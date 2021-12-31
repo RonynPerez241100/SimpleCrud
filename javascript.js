1d7b54cd541139d97060c6a4665a39098a79637d
ronynperez241100 <ronynperez@hotmail.com>
2021-12-30 21:36:43 -0600

    apply changes

--- javascript.js
+++ javascript.js
@@ -59,7 +59,7 @@ function updateRecord(formData) {
 }
 
 function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
+    if (confirm('Seguro que desea borrar esta celda ?')) {
         row = td.parentElement.parentElement;
         document.getElementById("employeeList").deleteRow(row.rowIndex);
         resetForm();
